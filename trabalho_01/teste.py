6c6df541f7ad807191453a4c114d4389e4f1851a
joao <joaovlcs07@gmail.com>
2022-02-12 15:44:26 -0300

    Adapted script for pure python

--- trabalho_01/teste.py
+++ trabalho_01/teste.py
@@ -17,7 +17,6 @@ Wish_list:
 
 
 # %% Tests and definitions
-import numpy as np
 
 
 # %% 
@@ -38,28 +37,27 @@ ca1.array_
 
 
 # list -> int -> np.array
-def circular_distance_array_from_idx(array: list,
-                                     indx: int) -> np.array:
-    """Calculate circular distance array from index."""
+def circular_distance_py(array: list,
+                         indx: int) -> list:
+    """Calculate circular distance array from index
+       in pure python."""
 
     n = len(array)
 
     array_indx = indx + 1
-    index_array = np.arange(start=1, stop=n+1, step=1)
+    index_array = list(range(1, n+1, 1))
 
-    forward_distance = np.abs(array_indx
-                              - index_array)
+    forward_distance = [abs(array_indx - i) for i in index_array]
+    backwards_distance = [n - i for i in forward_distance]
 
-    backwards_distance = n - forward_distance
-    circular_distance = np.min(np.array([forward_distance,
-                                         backwards_distance]),
-                               axis=0)
+    circular_distance = ([min([i, j]) for i, j
+                        in zip(forward_distance, backwards_distance)])
 
     return circular_distance
 
 # %% Read the input file
 
-input = open('cell1.in', 'r')
+input = open('cell.in', 'r')
 input_data = input.readlines()
 
 # %% Get params
@@ -72,17 +70,17 @@ i = int(parameters[3].replace('\n', ''))
 
 # %% Get the automaton
 
-automaton = np.array([int(numeric_string) for numeric_string in input_data[1].split(' ')])
+automaton =[int(numeric_string) for numeric_string in input_data[1].split(' ')]
 
-def get_d_environment_from_index(array: np.array,
-                                 index: int,
-                                 distance: int) -> np.array:
+def get_d_environment_py(array: list,
+                         index: int,
+                         d: int) -> list:
     """Get closest n elements from a circular array."""
 
-    distance_array = circular_distance_array_from_idx(array=array,
-                                                      indx=index)
-    distance_mask = distance_array <= distance
-    d_environment = array[distance_mask]
+    distances = circular_distance_py(array, index)
+
+    d_environment = ([element for element, distance in zip(array, distances)
+                              if distance <= d])
 
     return d_environment
 
@@ -91,19 +89,19 @@ result_array = automaton
 for key in range(i):
     print('--------------------')
     # Set the aux as an empty array
-    aux = np.array([])
+    aux = []
     for index in range(len(automaton)):
         print('Current automaton ', result_array)
-        neighbours = get_d_environment_from_index(array=result_array,
-                                                    index=index,
-                                                    distance=d)
+        neighbours = get_d_environment_py(array=result_array,
+                                          index=index,
+                                          d=d)
         if(len(neighbours) > 0):
-            aux = np.append(aux, int(np.sum(neighbours)%modular))
+            aux.append(sum(neighbours) % modular)
             print(f'Neighbours from position {index}: {neighbours}')
 
     result_array = aux[:]
 
-result_string = ' '.join(np.array([str(int(numeric)) for numeric in result_array]))
+result_string = ' '.join([str(int(numeric)) for numeric in result_array])
 print(result_string)
 
 
@@ -111,3 +109,4 @@ with open('cell.out', 'w+') as f:
     f.write(result_string)
 
 print(f'Result array: {result_array}')
+print(f'Steps: {i}')
